7ab0d0c66f95126801fe2d06dae8f74656f7a6b3
alextopand <erdekov.sascha@yandex.ru>
2024-11-29 20:32:12 +0300

    start models

---
+++ backend/profiles/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+from departments.models import *
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    description = models.TextField()
+    photo = models.ImageField(upload_to='profiles/', null=True, blank=True)
+    phone = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    role = models.ForeignKey(Role, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
