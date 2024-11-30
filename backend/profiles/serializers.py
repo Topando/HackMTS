4759db6895d9ed64488048374de7300da5a51330
alextopand <erdekov.sascha@yandex.ru>
2024-11-30 20:39:10 +0300

    cors

--- backend/profiles/serializers.py
+++ backend/profiles/serializers.py
@@ -4,6 +4,23 @@ from profiles.models import Employee
 
 
 class ProfileSerializer(serializers.ModelSerializer):
+    role_id_name = serializers.SerializerMethodField()
+    department_id_name = serializers.SerializerMethodField()
+
     class Meta:
         model = Employee
         fields = '__all__'
+
+    def get_role_id_name(self, obj):
+        if obj.role_id:
+            return {
+                'id': obj.role.id,
+                'name': obj.role.name,
+        }
+
+    def get_department_id_name(self, obj):
+        if obj.department_id:
+            return {
+                'id': obj.department.id,
+                'name': obj.department.name,
+            }
