6fc377dff8b095f4d9aa6e2276319d7a9f7f27c8
feoronis <dashaalimova2003@gmail.com>
2024-11-30 14:34:44 +0300

    Delete client directory

--- client/src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import UsersTable from "./(components)/UsersTable";
-
-export default function Home() {
-  return (
-    <UsersTable />
-  );
-}
--- client/src/app/structure/page.tsx
+++
@@ -1,7 +0,0 @@
-import UsersTable from "../(components)/UsersTable";
-
-export default function structure() {
-  return (
-    <div>test structure</div>
-  );
-}
